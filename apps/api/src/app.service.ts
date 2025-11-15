611ecce708034acc6a0dd50efab9cd048836343b
Mjnhdi35 <mjnhdi35@gmail.com>
2025-11-16 02:05:19 +0700

    update: nx and monorepo

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    // let a = '';
+    // a = 0;
     return 'Hello World!';
   }
 }
--- apps/web/app/app.vue
+++ apps/web/app/app.vue
@@ -1,3 +1,8 @@
 <template>
   <div>hello world</div>
 </template>
+
+<script setup lang="ts">
+// let a = ''
+// a = 0
+</script>
