d6f674b202c2c5289f08e5b8d12351ebcfe84315
Mjnhdi35 <mjnhdi35@gmail.com>
2025-11-16 03:27:43 +0700

    update tailwindcss color

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,9 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
   modules: ['@nuxt/ui', '@nuxt/eslint'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+
+  compatibilityDate: '2025-07-15',
 
   typescript: {
     strict: true,
@@ -13,4 +14,4 @@ export default defineNuxtConfig({
       stylistic: true,
     },
   },
-});
+})
